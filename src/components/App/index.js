55009c78aa1cd463ffba995dfda382b4e096b141
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-30 10:50:32 -0400

    add toggle functionality

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import {
   Box,
   Container,
-  FormControl,
   List,
   ListItem,
   ListItemText,
-  TextField
+  TextField,
+  makeStyles
 } from "@material-ui/core";
 import MyComponent from "components/MyComponent";
 
@@ -14,12 +14,12 @@ function TodoForm({ createTodo }) {
   const [task, setTask] = useState("");
 
   function handleChange(event) {
-    console.log("handleChange", event.target.value);
     setTask(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (task.trim() === "") return;
     createTodo(task);
     setTask("");
   }
@@ -35,17 +35,28 @@ function TodoForm({ createTodo }) {
   );
 }
 
-function TodoItem({ id, task, toggleTodo }) {
+function TodoItem({ id, task, isComplete, toggleTodo }) {
+  const classes = useStyles();
+
   function handleClick() {
     toggleTodo(id);
   }
 
   return (
     <ListItem button onClick={handleClick}>
-      <ListItemText primary={task} />
+      <ListItemText
+        primary={task}
+        className={isComplete ? classes.completed : ""}
+      />
     </ListItem>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  completed: {
+    textDecoration: "line-through"
+  }
+}));
 
 function App() {
   const [todos, setTodos] = useState([
@@ -62,13 +73,19 @@ function App() {
   ]);
 
   function createTodo(task) {
-    console.log("createTodo", task);
     const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
     setTodos(todos.concat({ id, task, isComplete: false }));
   }
 
   function toggleTodo(id) {
-    console.log("toggleTodo", id);
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      })
+    );
   }
 
   return (
@@ -83,7 +100,12 @@ function App() {
         <TodoForm createTodo={createTodo} />
         <List>
           {todos.map(todo => (
-            <TodoItem id={todo.id} task={todo.task} toggleTodo={toggleTodo} />
+            <TodoItem
+              id={todo.id}
+              task={todo.task}
+              isComplete={todo.isComplete}
+              toggleTodo={toggleTodo}
+            />
           ))}
         </List>
         {/* <MyComponent /> */}
