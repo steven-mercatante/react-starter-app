26703ba23ef788b627e9a6ef6ae5c946e8de922d
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-30 10:21:37 -0400

    add mock todo data, and stub functions

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Box, Container } from '@material-ui/core'
-import MyComponent from 'components/MyComponent'
+import React, { useState } from "react";
+import { Box, Container } from "@material-ui/core";
+import MyComponent from "components/MyComponent";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: "Start React project",
+      isComplete: true
+    },
+    {
+      id: 2,
+      task: "Organize party",
+      isComplete: false
+    }
+  ]);
+
+  function createTodo(task) {}
+
+  function toggleTodo(id) {}
+
   return (
     <Container>
       <Box
         my={20}
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-        flexDirection='row'
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="row"
       >
         <MyComponent />
       </Box>
