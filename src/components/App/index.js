0f5965c058b65631381436ff29189975ad38e05d
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-30 10:43:08 -0400

    prototyping functionality

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,51 @@
 import React, { useState } from "react";
-import { Box, Container } from "@material-ui/core";
+import {
+  Box,
+  Container,
+  FormControl,
+  List,
+  ListItem,
+  ListItemText,
+  TextField
+} from "@material-ui/core";
 import MyComponent from "components/MyComponent";
+
+function TodoForm({ createTodo }) {
+  const [task, setTask] = useState("");
+
+  function handleChange(event) {
+    console.log("handleChange", event.target.value);
+    setTask(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    createTodo(task);
+    setTask("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        placeholder="Enter Todo ..."
+        value={task}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+function TodoItem({ id, task, toggleTodo }) {
+  function handleClick() {
+    toggleTodo(id);
+  }
+
+  return (
+    <ListItem button onClick={handleClick}>
+      <ListItemText primary={task} />
+    </ListItem>
+  );
+}
 
 function App() {
   const [todos, setTodos] = useState([
@@ -16,9 +61,15 @@ function App() {
     }
   ]);
 
-  function createTodo(task) {}
+  function createTodo(task) {
+    console.log("createTodo", task);
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    setTodos(todos.concat({ id, task, isComplete: false }));
+  }
 
-  function toggleTodo(id) {}
+  function toggleTodo(id) {
+    console.log("toggleTodo", id);
+  }
 
   return (
     <Container>
@@ -29,7 +80,13 @@ function App() {
         alignItems="center"
         flexDirection="row"
       >
-        <MyComponent />
+        <TodoForm createTodo={createTodo} />
+        <List>
+          {todos.map(todo => (
+            <TodoItem id={todo.id} task={todo.task} toggleTodo={toggleTodo} />
+          ))}
+        </List>
+        {/* <MyComponent /> */}
       </Box>
     </Container>
   );
